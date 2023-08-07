3a5124824206de43d84252c808dc78d3ff9a606a
shoukatshah <m.shoukat@devbatch.com>
2023-08-07 18:51:54 +0500

    Login with Saudi Id, Iqama Id and MRN

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,18 +1,41 @@
 class Login {
     loginWithMRN(mrn,password){
-        cy.visit('/auth/user/dashboard',{timeout:60000})
         cy.get('.language-switcher').click()
-        cy.contains('Login').click();
-        // cy.get('#__BVID__22').click()
-        // cy.contains('Proceed with MRN').click()
-        cy.get('#__BVID__22').select('Proceed with MRN')
+        cy.contains('Login').click()
+        cy.get('.custom-select').select('Proceed with MRN')
         cy.get('[placeholder="Enter the ID Number"]').click()
         cy.get('[placeholder="Enter the ID Number"]').type(mrn)
         cy.get('[placeholder="Enter Password"]').click()
         cy.get('[placeholder="Enter Password"]').type(password)
         cy.contains('button','Login').click()
-        cy.url().should('eq','https://patientportal.almoosahospital.com.sa/dashboard')
+        cy.url().should('eq','https://stage-patientportal.almoosahospital.com.sa/dashboard')
     }
+
+    loginWithSaudiId(id) {
+        cy.get('.language-switcher').click()
+        cy.contains('Login').click()
+        cy.get('[placeholder="Enter the ID Number"]').click()
+        cy.get('[placeholder="Enter the ID Number"]').type(id)
+        cy.contains('button','Login').click()
+        cy.contains('Enter Verification Code').should('be.visible')
+        cy.contains('button','Verify').click()
+
+
+    }
+
+    loginWithIqamaId(id) {
+        cy.get('.language-switcher').click()
+        cy.contains('Login').click()
+        cy.get('.custom-select').select('Proceed with Iqama ID')
+        cy.get('[placeholder="Enter the ID Number"]').click()
+        cy.get('[placeholder="Enter the ID Number"]').type(id)
+        cy.contains('button','Login').click()
+        cy.contains('Enter Verification Code').should('be.visible')
+        cy.contains('button','Verify').click()
+
+
+    }
+
 }
 
 const login = new Login();
