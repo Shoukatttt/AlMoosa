f9128421e1d500ec20b21360332147fc6deca72f
shoukatshah <m.shoukat@devbatch.com>
2023-08-18 18:47:36 +0500

    updated the code for reports

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    debug:true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      screenshotOnRunFailure=true
+      screenshotOnRunFailure = true
       require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
